282576c0864a51c360cb37f96e3ed71159ca953c
lephisto <mephisto@mephis.to>
2019-03-27 14:46:44 +0100

    Implement functional Blacklist Script

--- roles/fastd/files/blacklist.sh
+++ roles/fastd/files/blacklist.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-exit 0
+#!/bin/bash
+PEER_KEY=$1
+PEER_ADDRESS=$2
+if /bin/grep -Fq $PEER_KEY /opt/ffrgb_cty/vpn-blacklist/blacklist.json; then
+    echo "BANNED KEY: $PEER_KEY $PEER_ADDRESS"
+    exit 1
+else
+    echo "CLEAN KEY: $PEER_KEY "
+    exit 0
+fi
