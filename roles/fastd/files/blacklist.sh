10738aa72142fd946cece1ddfbaabf0f2658ba39
Bastian Maeuser <mephisto@mephis.to>
2019-04-27 15:09:14 +0200

    Added IP to logging for helping mitigate Shortcuts

--- roles/fastd/files/blacklist.sh
+++ roles/fastd/files/blacklist.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 PEER_KEY=$1
 PEER_ADDRESS=$2
-if /bin/grep -Fq $PEER_KEY /opt/ffrgb_cty/vpn-blacklist/blacklist.json; then
+BLACKLIST=$3
+if /bin/grep -Fq $PEER_KEY $BLACKLIST; then
     echo "BANNED KEY: $PEER_KEY $PEER_ADDRESS"
     exit 1
 else
